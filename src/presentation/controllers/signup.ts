ecbbd8a9a46e7e8db5700d9d6992894d1c46f34c
“tiagoluis12” <tiago7164@hotmail.com>
2024-01-31 21:17:19 +0000

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,7 @@ import { type HttpRequest, type HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-erros'
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { ServerError } from '../errors/server-error'
-import { badRequest } from '../helpers/http-helpers'
+import { badRequest, serverError } from '../helpers/http-helpers'
 import { type Controller } from '../protocols/controller'
 import { type EmailValidator } from '../protocols/email-validator'
 
@@ -26,10 +26,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
