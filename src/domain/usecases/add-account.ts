ed346a0b4e576a73acc1fbe2bf066ad801679677
“tiagoluis12” <tiago7164@hotmail.com>
2024-02-02 18:05:33 +0000

    feat: ensure SignUpController calls AddAccount with correct values

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import { AccountModel } from '../models/account'
+
+export interface AddAccountModel {
+  name: string
+  email: string
+  password: string
+}
+
+export interface AddAccount {
+  add (account: AddAccountModel): AccountModel
+}
