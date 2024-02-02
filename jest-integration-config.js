60bc512ab6efc753d1b243a1ae03f37200153edd
“tiagoluis12” <tiago7164@hotmail.com>
2024-02-02 20:13:17 +0000

    chore: add jest configs and scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+const config = require('./jest-config')
+config.testMatch = ['**/*.test.ts']
+module.exports = config
---
+++ jest-unit-config.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+const config = require('./jest-config')
+config.testMatch = ['**/*.spec.ts']
+module.exports = config
