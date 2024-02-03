263e56a2c89e12719aa81c6d3c12e7158c3a7d4c
“tiagoluis12” <tiago7164@hotmail.com>
2024-02-03 15:40:12 +0000

    refactor: change file name

--- src/presentation/utils/email-validator-adapter.spec.ts
+++ src/presentation/utils/email-validator-adapter.spec.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { EmailValidatorAdapter } from './email-validator'
+import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 
 jest.mock('validator', () => ({
