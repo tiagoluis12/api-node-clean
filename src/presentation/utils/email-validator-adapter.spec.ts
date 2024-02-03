9e493b69c09cc9aa64dc389320eb798463a93d4b
“tiagoluis12” <tiago7164@hotmail.com>
2024-02-03 15:23:36 +0000

    test: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/presentation/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import { EmailValidatorAdapter } from './email-validator'
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@gmail.com')
+    expect(isValid).toBe(false)
+  })
+})
